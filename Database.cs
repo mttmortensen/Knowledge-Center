2a8b5d61b3731f6911225490e81ad93f350741cd
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-17 10:12:55 -0600

    added sqlclient to Database from Microsoft

--- Database.cs
+++ Database.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Knowledge_Center
 {
     public class Database
     {
+        private readonly string _connectionString;
+
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
         // === Connection Management ===
+
+        private SqlConnection OpenConnection() 
+        {
+            // Opens and returns a new SQL connection
+            throw new NotImplementedException();
+        }
 
         // === Executing Write Operations (INSERT, UPDATE, DELETE) ===
 
