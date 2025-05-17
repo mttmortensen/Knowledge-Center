997ba4fbbfafdc3871b99feaf6f0ee1e12857b7c
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-17 09:31:57 -0600

    Add project files.

---
+++ KnowledgeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center
+{
+    public class KnowledgeNode
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Domain { get; set; }
+        public string Description { get; set; }
+        public string ConfidenceLevel { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Knowledge_Center
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
