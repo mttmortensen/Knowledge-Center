f4b771282b239fe646ab38cfd7ea8a979c3a7005
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-17 22:34:18 -0600

    building out service for log entry. simple comments and construct setup

---
+++ LogEntryService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Knowledge_Center
+{
+    public class LogEntryService
+    {
+        private readonly Database _database;
+
+        public LogEntryService(Database database)
+        {
+            _database = database;
+        }
+
+        /* ===================== CRUD ===================== */
+
+        // === CREATE ===
+        // === READ ===
+        // === UPDATE ===
+        // === DELETE ===
+
+        /* ===================== DATA TYPE CONVERTERS (MAPPERS) ===================== */
+    }
+}
