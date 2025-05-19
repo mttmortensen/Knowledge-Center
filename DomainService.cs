6b17cd89de5fb10aea54a287aaba148dd9fc3601
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-19 15:43:24 -0600

    basic outline of domain service and it's operations

---
+++ DomainService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center
+{
+    public class DomainService
+    {
+        private readonly Database _database;
+        public DomainService(Database database) 
+        {
+            _database = database;
+        }
+
+        public bool CreateDomain(Domain domain);
+        public List<Domain> GetAllDomains();
+        public Domain GetDomainById(int domainId);
+        public bool UpdateDomain(Domain domain);
+        public bool DeleteDomain(int domainId);
+    }
+}
