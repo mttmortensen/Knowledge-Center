8f185eabe4ccd0b9d5924786ae420e4d151466b9
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-20 09:22:47 -0600

    contr made for KN. Just header for GetAll()

---
+++ API/Controllers/KnowledgeNodeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Knowledge_Center.Services;
+
+namespace Knowledge_Center.API.Controllers
+{
+    public class KnowledgeNodeController
+    {
+        private readonly KnowledgeNodeService _knowledgeNodeService;
+
+        public KnowledgeNodeController(KnowledgeNodeService knService) 
+        {
+            _knowledgeNodeService = knService;
+        }
+
+        // === GET /api/knowledge-nodes ===
+        public void GetAll(HttpListenerResponse response) 
+        {
+            //Get All Knowledge Nodes
+            throw new NotImplementedException();
+        }
+    }
+}
