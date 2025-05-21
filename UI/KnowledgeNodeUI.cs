70f2b3eefb6b6fb9a167e53c5f847c4c4254fd46
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-21 10:02:42 -0600

    Breaking down the UI for V2! Starting with KNs. Here's base UI for it

---
+++ UI/KnowledgeNodeUI.cs
@@ -0,0 +1,28 @@
+﻿using Knowledge_Center.Models;
+using Knowledge_Center.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center.UI
+{
+    public class KnowledgeNodeUI
+    {
+        private readonly KnowledgeNodeService _knService;
+        private readonly LogEntryService _lgService;
+        private readonly DomainService _dnService;
+
+
+        public KnowledgeNodeUI(KnowledgeNodeService knService, LogEntryService lgService, DomainService dnService) 
+        {
+            _knService = knService;
+            _lgService = lgService;
+            _dnService = dnService;
+
+        }
+
+
+    }
+}
