28c8deb9e402b23b8e913c87e2b9356ae23484b1
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-21 10:33:55 -0600

    logEntryUI created with constructor

---
+++ UI/LogEntryUI.cs
@@ -0,0 +1,23 @@
+﻿using Knowledge_Center.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center.UI
+{
+    public class LogEntryUI
+    {
+        private readonly KnowledgeNodeService _knService;
+        private readonly LogEntryService _lgService;
+        private readonly DomainService _dnService;
+
+        public LogEntryUI(KnowledgeNodeService knService, LogEntryService lgService, DomainService dnService)
+        {
+            _knService = knService;
+            _lgService = lgService;
+            _dnService = dnService;
+        }
+    }
+}
