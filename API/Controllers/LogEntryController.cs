1efb0463ada0dfa799cf33c5eaae4b7a19410dce
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-23 14:13:44 -0600

    controller now for api for log entry.

---
+++ API/Controllers/LogEntryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Knowledge_Center.Models;
+using Knowledge_Center.Services;
+
+namespace Knowledge_Center.API.Controllers
+{
+    public class LogEntryController
+    {
+        private readonly LogEntryService _lgService;
+
+        public LogEntryController(LogEntryService lgService)
+        {
+            _lgService = lgService;
+        }
+
+        // === GET /api/logs ===
+
+        // === GET /api/logs/{id} ===
+
+        // === POST /api/logs ===
+    }
+}
