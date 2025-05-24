407466776e62d3c64853536f599acf4818c2ec49
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-24 16:23:39 -0600

    now bringing in tag service

---
+++ Services/TagService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Knowledge_Center.Models;
+
+namespace Knowledge_Center.Services
+{
+    public class TagService
+    {
+        private readonly Database _db;
+        public TagService(Database db) 
+        {
+            _db = db;
+        }
+    }
+}
