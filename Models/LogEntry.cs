506e8562b2729d4303f5b442b071651efb561cd1
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-24 16:10:27 -0600

    linking a tag to a logEntry via tagId

--- Models/LogEntry.cs
+++ Models/LogEntry.cs
@@ -12,6 +12,7 @@ namespace Knowledge_Center.Models
         public int NodeId { get; set; }
         public DateTime EntryDate { get; set; }
         public string Content { get; set; }
+        public int TagId { get; set; }
         public string Tag { get; set; }
         public bool ContributesToProgress { get; set; }
 
