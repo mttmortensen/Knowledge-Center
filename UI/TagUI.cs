2a9e5fbeee033671105866e3eea2ec4706f4ed3b
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-24 20:34:42 -0600

    TagUI menu introduced will link up back to main shortly

---
+++ UI/TagUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Knowledge_Center.Services;
+
+namespace Knowledge_Center.UI
+{
+    public class TagUI
+    {
+        private readonly TagService _tagService;
+
+        public TagUI(TagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        // ========================== MAIN MENU ==========================
+        public void ShowTagMenu()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("=== TAG MANAGEMENT ===");
+                Console.WriteLine("1. Create New Tag");
+                Console.WriteLine("2. View All Tags");
+                Console.WriteLine("0. Back to Main Menu");
+                Console.Write("\nSelect an option: ");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        CreateTag();
+                        break;
+                    case "2":
+                        ViewAllTags();
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        break;
+                }
+            }
+        }
+        // ========================== CRUD ==========================
+    }
+}
