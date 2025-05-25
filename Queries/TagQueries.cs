83815ba63dc0f814ee24c245d823939e17c0dcd1
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-24 20:28:04 -0600

    tag query class for data maniuplation on tag table

---
+++ Queries/TagQueries.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center.Queries
+{
+    public static class TagQueries
+    {
+        public static readonly string InsertTag = @"
+            INSERT INTO Tags 
+                (TName)
+            VALUES 
+                (@TName);
+        ";
+    }
+}
