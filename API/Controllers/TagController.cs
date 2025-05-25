5267d3b932cf45199e3bb0a36d53cd873bbee794
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-24 23:02:57 -0600

    now working on API controller for Tags

---
+++ API/Controllers/TagController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Knowledge_Center.Services;
+
+namespace Knowledge_Center.API.Controllers
+{
+    public class TagController
+    {
+        private readonly TagService _tagService;
+
+        public TagController(TagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        // === GET /api/tags ===
+        // === GET /api/tags/{id} ===
+        // === POST /api/tags ===
+        // === PUT /api/tags/{id} ===
+        // === DELETE /api/tags/{id} ===
+
+        // === HELPER ===
+    }
+}
