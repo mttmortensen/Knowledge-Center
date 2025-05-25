e55904b1071adda0adaff8629be4052d9ef84c72
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-24 23:19:45 -0600

    crud methods for get and post done. and helper method made

--- API/Controllers/TagController.cs
+++ API/Controllers/TagController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Knowledge_Center.Services;
+using Knowledge_Center.Models;
 
 namespace Knowledge_Center.API.Controllers
 {
@@ -17,11 +20,76 @@ namespace Knowledge_Center.API.Controllers
         }
 
         // === GET /api/tags ===
+        public void GetAll(HttpListenerResponse response)
+        {
+            // Get All Tags
+            List<Models.Tags> tags = _tagService.GetAllTags();
+            
+            // Convert to JSON
+            WriteJson(response, HttpStatusCode.OK, tags);
+        }
+
         // === GET /api/tags/{id} ===
+        public void GetById(HttpListenerResponse response, int id)
+        {
+            var tag = _tagService.GetTagById(id);
+
+            if (tag == null)
+            {
+                response.StatusCode = (int)HttpStatusCode.NotFound;
+                return;
+            }
+
+            // Convert to JSON
+            WriteJson(response, HttpStatusCode.OK, tag);
+        }
+
         // === POST /api/tags ===
+        public void Create(HttpListenerResponse response, HttpListenerRequest request)
+        {
+            using var reader = new StreamReader(request.InputStream, request.ContentEncoding);
+            string body = reader.ReadToEnd();
+
+            Tags tag;
+            
+            try
+            {
+                tag = JsonSerializer.Deserialize<Models.Tags>(body);
+            }
+            catch (JsonException)
+            {
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+
+            bool success = _tagService.CreateTag(tag);
+            if (success)
+            {
+
+                response.StatusCode = (int)HttpStatusCode.Created;
+                WriteJson(response, HttpStatusCode.Created, tag);
+            }
+            else
+            {
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+            
+        }
         // === PUT /api/tags/{id} ===
         // === DELETE /api/tags/{id} ===
 
         // === HELPER ===
+        private void WriteJson(HttpListenerResponse response, HttpStatusCode statusCode, object obj)
+        {
+            string json = JsonSerializer.Serialize(obj);
+            byte[] buffer = Encoding.UTF8.GetBytes(json);
+
+            response.StatusCode = (int)statusCode;
+            response.ContentType = "application/json";
+            response.ContentLength64 = buffer.Length;
+
+            using Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+        }
     }
 }
