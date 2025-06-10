4910fa5c596a8e276cb2c90561bd5619d1844608
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-10 14:40:16 -0600

    creation of InputValidator and KN headers

---
+++ Services/Validation/InputValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center.Services.Validation
+{
+    public class InputValidator
+    {
+        private static readonly HashSet<string> ValidNodeTypes = new(){ "Concept", "Project" };
+        private static readonly HashSet<string> ValidStatuses  = new(){ "Exploring", "Learning", "Mastered" };
+
+        public static void ValidateTitle(string title) { }
+        public static void ValidateDescription(string description) { }
+        public static void ValidateNodeType(string nodeType) { }
+        public static void ValidateStatus(string status) { }
+    }
+}
