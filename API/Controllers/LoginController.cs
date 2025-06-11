083e5e911cf3c7deb5ec18ccdf094bc6e5f40399
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-11 16:22:28 -0600

    still need a login controller for handling post reqs from api

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center.API.Controllers
+{
+    public class LoginController
+    {
+        public void HandleLogin(HttpListenerResponse response, HttpListenerRequest request) { }
+        public void HandleLogout(HttpListenerResponse response, HttpListenerRequest request) { }
+
+    }
+}
