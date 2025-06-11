6d9f7382487f2f82e3134af9154dd00af799f36e
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-11 16:03:19 -0600

    adding the auth session class for handling session based auths/logins

---
+++ Services/Security/AuthSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center.Services.Security
+{
+    public class AuthSession
+    {
+        // username -> token
+        private static Dictionary<string, string> _activeSessions = new();
+
+        public static string CreateSession(string username) { }
+
+        public static bool IsValidToken(string token) { }
+
+        public static void EndSession(string username) { }
+    }
+}
