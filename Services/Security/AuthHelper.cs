826af847024eebae1c690bc8cfe4513e8491aff0
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-11 22:36:29 -0600

    adding new class to help with verifing what route now need auth token before proceeding

---
+++ Services/Security/AuthHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Center.Services.Security
+{
+    public class AuthHelper
+    {
+        public static bool RequireAuth(HttpListenerRequest request, HttpListenerResponse response) 
+        {
+
+        }
+    }
+}
